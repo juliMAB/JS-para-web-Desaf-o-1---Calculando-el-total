d42d869b925595d06c2cc1a92928ef95b5debda2
juliMAB <julian6aguirre@hotmail.com>
2025-08-21 21:10:02 -0300

    Añadir restar y updatear los valores en el html.
    todo el funcionamiento basico para mostrar el precio total y la cantidad funcional.

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
+/* Inicialización de variables */
+precio = 400000
+
+precioSpan = document.querySelector(".precio-inicial");
+precioSpan.innerHTML = precio
+/* Fin de los recursos */
 
 
 let element_cantidad = document.querySelector(".cantidad");
 let element_precio_inicial = document.querySelector(".precio-inicial");
-let element_precio_total = document.querySelector(".precio-total");
+let element_precio_total = document.querySelector(".valor-total");
 
 let cantidad = element_cantidad.innerHTML ? parseInt(element_cantidad.innerHTML) : 0;
 let precioInicial = element_precio_inicial.innerHTML ? parseFloat(element_precio_inicial.innerHTML) : 0;
@@ -13,5 +19,22 @@ function onClickButtonAdd()
 {
     cantidad++;
     element_cantidad.innerHTML = cantidad.toString();
-    element_precio_total.innerHTML = precioTotal;
+    UpdatePriceTotal();
+}
+
+
+function onClickButtonSubtract()
+{
+    if (cantidad > 0) {
+        cantidad--;
+        element_cantidad.innerHTML = cantidad.toString();
+        UpdatePriceTotal();
+    }
+}
+
+
+function UpdatePriceTotal()
+{
+    precioTotal = cantidad * precioInicial;
+    element_precio_total.innerHTML = precioTotal.toString();
 }
