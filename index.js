f5bd88f0ff5f0a2eb5c7519b912bcbbbe6411a34
juliMAB <julian6aguirre@hotmail.com>
2025-08-21 21:04:02 -0300

    Añadir funcionalidad de sumar al boton de suma.
    Solo para sumar la cantidad y entender la forma correcta de obtener valores del html y modificar sus valores.

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-precio = 400000
 
-precioSpan = document.querySelector(".precio-inicial");
-precioSpan.innerHTML = precio
+
+let element_cantidad = document.querySelector(".cantidad");
+let element_precio_inicial = document.querySelector(".precio-inicial");
+let element_precio_total = document.querySelector(".precio-total");
+
+let cantidad = element_cantidad.innerHTML ? parseInt(element_cantidad.innerHTML) : 0;
+let precioInicial = element_precio_inicial.innerHTML ? parseFloat(element_precio_inicial.innerHTML) : 0;
+let precioTotal = element_precio_total.innerHTML ? parseFloat(element_precio_total.innerHTML) : 0;
+
+
+function onClickButtonAdd()
+{
+    cantidad++;
+    element_cantidad.innerHTML = cantidad.toString();
+    element_precio_total.innerHTML = precioTotal;
+}
